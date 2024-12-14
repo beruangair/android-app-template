bc8f78ab57a9a8ec8c243b146877bf9a9a4729a7
Arif Lutfhansah <aplikasi.arif@gmail.com>
2024-12-14 19:37:59 +0700

    Tambah Fungsi Ubah Tema

--- app/src/main/java/com/ariflutfhansah/template/MainActivity.kt
+++ app/src/main/java/com/ariflutfhansah/template/MainActivity.kt
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ariflutfhansah.template.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGImageView
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    // Ubah Tema
+    private lateinit var themeSelect: SVGImageView
+    private lateinit var sharedPreferences: SharedPreferences
 
     var toolbar: Toolbar? = null
 
@@ -35,5 +42,46 @@ class MainActivity : AppCompatActivity() {
         //setupActionBarWithNavController(navController, appBarConfiguration)
         setSupportActionBar(toolbar)
         navView.setupWithNavController(navController)
+
+        // Memanggil Image SVG
+        val logoApp: SVGImageView = findViewById(R.id.logo_app)
+        val theme_select: SVGImageView = findViewById(R.id.theme_select)
+
+        val svg1: SVG = SVG.getFromResource(resources, R.raw.icon_masakpedia)
+        logoApp.setSVG(svg1)
+
+        val svg2: SVG = SVG.getFromResource(resources, R.raw.theme_light)
+        theme_select.setSVG(svg2)
+
+        // Ubah Tema
+        sharedPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE)
+        val isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false)
+
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        themeSelect = findViewById(R.id.theme_select)
+
+        // Mengatur listener untuk mengubah tema saat ikon diklik
+        themeSelect.setOnClickListener {
+            toggleTheme()
+        }
+    }
+
+    private fun toggleTheme() {
+        // Mengubah tema
+        val isDarkTheme = sharedPreferences.getBoolean("isDarkTheme", false)
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            sharedPreferences.edit().putBoolean("isDarkTheme", false).apply()
+            themeSelect.setSVG(SVG.getFromResource(resources, R.raw.theme_light)) // Ganti dengan ikon light
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            sharedPreferences.edit().putBoolean("isDarkTheme", true).apply()
+            themeSelect.setSVG(SVG.getFromResource(resources, R.raw.theme_dark)) // Ganti dengan ikon dark
+        }
     }
 }
